c1bba8d878e18dd45c81e4e7dd8da53f8a889d0b
Welcome ! <usmanubaid9@gmail.com>
2024-02-28 20:51:33 +0100

    feat: use jwt lib

---
+++ server/src/services/auth.ts
@@ -0,0 +1,26 @@
+import bcrypt from "bcrypt";
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+const saltRounds = 10;
+const jwtSecret = process.env.JWT_SECRET;
+
+export const hashPassword = (password: string) => {
+  return bcrypt.hash(password, saltRounds);
+};
+
+export const comparePasswords = (userPassword: string, dbPassword: string) => {
+  return bcrypt.compare(userPassword, dbPassword);
+};
+
+export const generateJWT = (id: string) => {
+  if (jwtSecret) {
+    return jwt.sign({ id }, jwtSecret);
+  }
+};
+
+export const verifyJWT = (token: string) => {
+  if (jwtSecret) {
+    const decode = jwt.verify(token, jwtSecret);
+    return decode as JwtPayload;
+  }
+};
