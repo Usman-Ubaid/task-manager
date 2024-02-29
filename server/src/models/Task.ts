ff140603be5fb781dce40ffad33760b84f608003
Welcome ! <usmanubaid9@gmail.com>
2024-02-29 10:27:30 +0100

    feat: task schema

---
+++ server/src/models/Task.ts
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+import User from "./User";
+
+const taskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    default: "",
+  },
+  priority: {
+    type: String,
+    enum: ["Low", "Medium", "High"],
+    default: "Medium",
+  },
+  dueDate: {
+    type: Date,
+    required: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: User,
+    required: true,
+  },
+});
+
+const Task = mongoose.model("Task", taskSchema);
+
+module.exports = Task;
