d269c0a99c13f85b2621a0c3315d1e19ac9d6831
Welcome ! <usmanubaid9@gmail.com>
2024-03-01 21:47:40 +0100

    refactor: handle errors

--- server/src/utils/ApiError.ts
+++ server/src/utils/ApiError.ts
@@ -1,5 +1,13 @@
 import { ErrorRequestHandler } from "express";
 
+export class ApiError extends Error {
+  statusCode: number;
+  constructor(message: string, statusCode: number) {
+    super(message);
+    this.statusCode = statusCode;
+  }
+}
+
 export const apiErrorHandler: ErrorRequestHandler = (error, req, res, next) => {
-  return res.status(400).json({ error: error.message });
+  return res.status(error.statusCode).json({ error: error.message });
 };
